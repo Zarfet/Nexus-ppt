73e7e2b433d6030d89dbc18ed38f37a472c305bc
Ilverzon Zarate <ilverzonzarate@gmail.com>
2026-06-30 20:22:20 +0200

    Revert "Add Pusher-based remote speaker notes sync"
    This reverts commit 0c15adf45838a5e5a3664ad4d95a12d8e65e32e3.

--- api/config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.status(200).send(
-    'window.__PUSHER_KEY__ = ' + JSON.stringify(process.env.PUSHER_KEY || '') + ';\n' +
-    'window.__PUSHER_CLUSTER__ = ' + JSON.stringify(process.env.PUSHER_CLUSTER || '') + ';\n'
-  );
-}
