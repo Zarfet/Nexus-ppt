d3157fde9362daac2fd32f08716ea0adb4b6beae
Ilverzon Zarate <ilverzonzarate@gmail.com>
2026-06-30 19:20:49 +0200

    Revert "Add Pusher-based live speaker notes sync for phone"
    This reverts commit a34b172f5280aaecae3a5756ebd90c7a1518e7e0.

--- party/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  onConnect(connection, room) {
-    connection.addEventListener("message", (event) => {
-      // Relay to all other connected clients
-      room.broadcast(event.data, [connection.id]);
-    });
-  }
-};
